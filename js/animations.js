1ce305b8eb329c498210f3c71582de99fb642ae0
Andrea Calciolari <andrea.calciolari@pwc.com>
2025-08-22 11:32:39 +0200

    Work on animations

--- js/animations.js
+++ js/animations.js
@@ -1,79 +1,19 @@
-const textContainer = Array.from(document.querySelectorAll('div.text-container:not(.text-container-manual)'));
-const buttons = Array.from(document.querySelectorAll('.button-animation'));
-const bannerGalleryContainer = Array.from(document.querySelectorAll('.gallery-container'));
-const fadeinAnimation = Array.from(document.querySelectorAll('.fade-in-animation:not(.fade-in-animation-manual)'));
 const verticalText = Array.from(document.querySelectorAll('main > div [class$="-wrapper"]'));
-const backgroundGallery = Array.from(document.querySelectorAll('.banner-background-gallery .background-gallery-container'));
+const blockWrapper = Array.from(document.querySelectorAll('main > div [class$="-wrapper"]'));
 
 window.addEventListener('scroll', () => {
   scrollEvent();
 });
 
 function scrollEvent() {
-  for (let i = 0; i < textContainer.length; i++) {
-    const el = textContainer[i];
+  for (let i = 0; i < blockWrapper.length; i++) {
+    const el = blockWrapper[i];
     const rect = el.getBoundingClientRect();
-    const isInView = rect.top <= (window.innerHeight / 1.5) && rect.bottom >= 0;
+    const isInView = rect.top <= (window.innerHeight / 4) && rect.bottom >= 0;
 
-    if (isInView && !el.children[0].classList.contains('visible')) {
-      setTimeout(() => {
-        el.children[0].classList.add('visible');
-      }, el.children[0].getAttribute('data-delayed') === 'true' ? 1500 : 0);
-      textContainer.splice(i, 1);
-      i--;
-    }
-  }
-
-  for (let i = 0; i < buttons.length; i++) {
-    const el = buttons[i];
-    const rect = el.getBoundingClientRect();
-    const isInView = rect.top <= (window.innerHeight / 1.5) && rect.bottom >= 0;
-
-    if (isInView && !el.classList.contains('visible')) {
-      setTimeout(() => {
-        el.classList.add('visible');
-      }, el.getAttribute('data-delayed') === 'true' ? 1500 : 0);
-      buttons.splice(i, 1);
-      i--;
-    }
-  }
-
-  for (let i = 0; i < bannerGalleryContainer.length; i++) {
-    const el = bannerGalleryContainer[i];
-    const rect = el.getBoundingClientRect();
-    const isInView = rect.top <= (window.innerHeight / 1.5) && rect.bottom >= 0;
-
-    if (isInView && !el.classList.contains('visible')) {
-      const children = el.querySelectorAll('.photo-gallery-item');
-      let currentIndex = 0;
-      const timeout = el.classList.contains('gallery-fast-animation') ? 600 : 1000;
-      function showNextImage() {
-        if (currentIndex < children.length) {
-          const currentImage = children[currentIndex];
-          currentImage.classList.add('visible');
-
-          setTimeout(() => {
-            currentIndex++;
-            showNextImage();
-          }, timeout);
-        }
-      }
-      showNextImage();
-      bannerGalleryContainer.splice(i, 1);
-      i--;
-    }
-  }
-
-  for (let i = 0; i < fadeinAnimation.length; i++) {
-    const el = fadeinAnimation[i];
-    const rect = el.getBoundingClientRect();
-    const isInView = rect.top <= (window.innerHeight / 1.5) && rect.bottom >= 0;
-
-    if (isInView && !el.classList.contains('visible')) {
-      setTimeout(() => {
-        el.classList.add('visible');
-      }, el.getAttribute('data-delayed') ? +el.getAttribute('data-delayed') : 0);
-      fadeinAnimation.splice(i, 1);
+    if (isInView) {
+      scrollElements(el);
+      blockWrapper.splice(i, 1);
       i--;
     }
   }
@@ -90,32 +30,79 @@ function scrollEvent() {
       text.textContent = child ? child.getAttribute('data-vertical-text') : '';
     }
   }
+}
+
+function scrollElements(block) {
+  const textContainer = Array.from(block.querySelectorAll('div.text-container:not(.text-container-manual)'));
+  const buttons = Array.from(block.querySelectorAll('.button-animation'));
+  const bannerGalleryContainer = Array.from(block.querySelectorAll('.gallery-container'));
+  const fadeinAnimation = Array.from(block.querySelectorAll('.fade-in-animation:not(.fade-in-animation-manual)'));
+  const backgroundGallery = Array.from(block.querySelectorAll('.banner-background-gallery .background-gallery-container'));
+
+  for (let i = 0; i < textContainer.length; i++) {
+    const el = textContainer[i];
+    if (!el.children[0].classList.contains('visible')) {
+      setTimeout(() => {
+        el.children[0].classList.add('visible');
+      }, el.children[0].getAttribute('data-delayed') === 'true' ? 1500 : 0);
+    }
+  }
+
+  for (let i = 0; i < buttons.length; i++) {
+    const el = buttons[i];
+    if (!el.classList.contains('visible')) {
+      setTimeout(() => {
+        el.classList.add('visible');
+      }, el.getAttribute('data-delayed') === 'true' ? 1500 : 0);
+    }
+  }
+
+  for (let i = 0; i < bannerGalleryContainer.length; i++) {
+    const el = bannerGalleryContainer[i];
+    if (!el.classList.contains('visible')) {
+      const children = el.querySelectorAll('.photo-gallery-item');
+      let currentIndex = 0;
+      const timeout = el.classList.contains('gallery-fast-animation') ? 600 : 1000;
+      function showNextImage() {
+        if (currentIndex < children.length) {
+          const currentImage = children[currentIndex];
+          currentImage.classList.add('visible');
+
+          setTimeout(() => {
+            currentIndex++;
+            showNextImage();
+          }, timeout);
+        }
+      }
+      showNextImage();
+    }
+  }
+
+  for (let i = 0; i < fadeinAnimation.length; i++) {
+    const el = fadeinAnimation[i];
+    if (!el.classList.contains('visible')) {
+      setTimeout(() => {
+        el.classList.add('visible');
+      }, el.getAttribute('data-delayed') ? +el.getAttribute('data-delayed') : 0);
+    }
+  }
 
   for (let i = 0; i < backgroundGallery.length; i++) {
     const el = backgroundGallery[i];
-    const rect = el.getBoundingClientRect();
-    const isInView = rect.top <= (window.innerHeight / 1.5) && rect.bottom >= 0;
+    const galleryItems = el.querySelectorAll('.background-gallery-item');
+    for (let i = 0; i < galleryItems.length; i++) {
+      const item = galleryItems[i];
 
-    if (isInView) {
-
-      const galleryItems = el.querySelectorAll('.background-gallery-item');
-      for (let i = 0; i < galleryItems.length; i++) {
-        const item = galleryItems[i];
-
-        let scrollSpeed = 0;
-        function scrollElement() {
-          item.style.transform = `translateY(${scrollSpeed}px)`;
-          if (i % 2 == 0)
-            scrollSpeed--;
-          else
-            scrollSpeed++;
-          if (item.getBoundingClientRect().bottom <= 0) clearInterval(scrollInterval);
-        }
-        const scrollInterval = setInterval(scrollElement, 20);
+      let scrollSpeed = 0;
+      function scrollElement() {
+        item.style.transform = `translateY(${scrollSpeed}px)`;
+        if (i % 2 == 0)
+          scrollSpeed--;
+        else
+          scrollSpeed++;
+        if (item.getBoundingClientRect().bottom <= 0) clearInterval(scrollInterval);
       }
-
-      backgroundGallery.splice(i, 1);
-      i--;
+      const scrollInterval = setInterval(scrollElement, 20);
     }
   }
 }
